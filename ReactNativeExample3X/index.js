f51b03616ef99615f6fa8dd07d0a1c0e878b585d
Daniel <daniel.f@taboola.com>
2022-11-27 17:41:43 +0200

    init project

--- ReactNativeExample3X/index.js
+++ ReactNativeExample3X/index.js
@@ -5,5 +5,9 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import { Taboola} from '@taboola/rnt-dev'
+
+//init Taboola with publisher ID
+Taboola.init('sdk-tester-demo');
 
 AppRegistry.registerComponent(appName, () => App);
