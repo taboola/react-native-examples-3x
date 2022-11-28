1188507c561c0f7d9e6e3a5d8a7f30712df5a7a4
Daniel <daniel.f@taboola.com>
2022-11-28 11:21:24 +0200

    Navigation added

--- ReactNativeExample3X/App.js
+++ ReactNativeExample3X/App.js
@@ -1,22 +1,88 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import * as React from 'react';
+import { Button, View } from 'react-native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
-import ArticleWithWidgetInFlatList from "./src/screens/ArticleWithWidgetInFlatList";
-
-
-const App = () => {
-  return(
-      <ArticleWithWidgetInFlatList/>
-  )
+function Widget() {
+  return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        {/* eslint-disable-next-line react/jsx-no-undef */}
+        <ArticleWithWidgetInFlatList />
+      </View>
+  );
+}
+function Feed() {
+  return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <FeedWithArticle />
+      </View>
+  );
 }
 
+
+
+
+function Main() {
+  const navo = useNavigation();
+  return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Button
+            title={'Feed'}
+            onPress={() => {
+              navo.navigate('Feed');
+            }}
+        />
+        <Button
+            title={'Widget'}
+            onPress={() => {
+              navo.navigate('Widget');
+            }}
+        />
+
+
+        <Button
+            title={'Tabs Screen'}
+            onPress={() => {
+              navo.navigate('Tabs');
+            }}
+        />
+
+      </View>
+  );
+}
+
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import ArticleWithWidgetInFlatList from "./src/screens/ArticleWithWidgetInFlatList";
+import FeedWithArticle from "./src/screens/FeedWithArticle";
+
+
+const Tab = createMaterialTopTabNavigator();
+
+function MyTabs() {
+  return (
+      <Tab.Navigator>
+        <Tab.Screen name="1" component={Feed} />
+        <Tab.Screen name="2" component={Feed} />
+        <Tab.Screen name="3" component={Feed} />
+      </Tab.Navigator>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Main" component={Main} />
+          <Stack.Screen name="Widget" component={Widget} />
+          <Stack.Screen name="Feed" component={Feed} />
+          <Stack.Screen name="LimiteFeed" component={Feed} />
+          <Stack.Screen name="Tabs" component={MyTabs} />
+        </Stack.Navigator>
+      </NavigationContainer>
+  );
+}
 
 export default App;
